d7aa7e31e939bf7b2e6bfb3ac6fec4afc6adae62
mrkaskar <kzinthant.d3v@gmail.com>
2024-10-21 16:40:25 +0000

    refactor: refactor structure

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 
-use ztp::run;
+use ztp::startup::run;
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
