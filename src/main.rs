4bd4954b904e2e1b290c53b0cfbf53601cc632d8
mrkaskar <kzinthant.d3v@gmail.com>
2024-10-21 16:47:51 +0000

    feat: config added

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::net::TcpListener;
 
-use ztp::startup::run;
+use ztp::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = TcpListener::bind(address).expect("Failed to bind random port");
+
     run(listener)?.await
 }
